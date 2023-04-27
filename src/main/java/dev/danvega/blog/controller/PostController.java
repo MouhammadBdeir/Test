cb79003eb7f2abf5b087ef00d3d60ea7323a8099
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-04-27 14:15:38 +0200

    test 04

--- src/main/java/dev/danvega/blog/controller/PostController.java
+++ src/main/java/dev/danvega/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import dev.danvega.blog.repository.AuthorRepository;
 import dev.danvega.blog.repository.PostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,5 +41,74 @@ public class PostController {
         LOG.info("Post Details called for: " + post.getId());
         return new PostDetails(post,authors.findById(post.getAuthor().getId()).get());
     }
-
+    @GetMapping("/index")
+    public String showForm(Model model) {
+        System.out.println("showForm sucess ");
+        return "index";
+    }
+    @GetMapping("/projects")
+    public String showProjects(Model model) {
+        System.out.println("showProjects sucess ");
+        return "Projekte";
+    }
+    @GetMapping("/QuS")
+    public String showQuS(Model model) {
+        System.out.println("showQuS sucess");
+        return "QuS";
+    }
+    @GetMapping("/Auditierung")
+    public String showAuditierung(Model model) {
+        System.out.println("showAuditierung sucess");
+        return "Auditierung";
+    }
+    @GetMapping("/Beratung")
+    public String showBeratung(Model model) {
+        System.out.println("showBeratung sucess");
+        return "Beratung";
+    }
+    @GetMapping("/HuST")
+    public String showHuST(Model model) {
+        System.out.println("showHuST sucess");
+        return "HuST";
+    }
+    @GetMapping("/Engineering")
+    public String showEngineering(Model model) {
+        System.out.println("showEngineering sucess");
+        return "Engineering";
+    }
+    @GetMapping("/Programierung")
+    public String showProgramierung(Model model) {
+        System.out.println("showProgramierung sucess");
+        return "Programierung";
+    }
+    @GetMapping("/Akustik")
+    public String showAkustik(Model model) {
+        System.out.println("showAkustik sucess");
+        return "Akustik";
+    }
+    @GetMapping("/SHuK")
+    public String showSHuK(Model model) {
+        System.out.println("showSHuK sucess");
+        return "SHuK";
+    }
+    @GetMapping("/Kontakt")
+    public String showKontakt(Model model) {
+        System.out.println("showKontakt sucess");
+        return "Kontakt";
+    }
+    @GetMapping("/impressum")
+    public String showImpressum(Model model) {
+        System.out.println("showImpressum sucess");
+        return "impressum";
+    }
+    @GetMapping("/referenzen")
+    public String showReferenzen(Model model) {
+        System.out.println("showReferenzen sucess");
+        return "referenzen";
+    }
+    @GetMapping("/test")
+    public String showTest(Model model) {
+        System.out.println("showTest sucess");
+        return "test";
+    }
 }
