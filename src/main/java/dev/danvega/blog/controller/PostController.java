b5f42541107d1eeeef218211d3673e71ccf973a9
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-04-27 14:30:09 +0200

    controller changed

--- src/main/java/dev/danvega/blog/controller/PostController.java
+++ src/main/java/dev/danvega/blog/controller/PostController.java
@@ -6,13 +6,14 @@ import dev.danvega.blog.repository.AuthorRepository;
 import dev.danvega.blog.repository.PostRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class PostController {
 
