2546b882a6252d33dbdbb7750ae3b3a844d747ad
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-04-28 02:20:15 +0200

    test 06

--- src/main/java/dev/danvega/blog/Application.java
+++ src/main/java/dev/danvega/blog/Application.java
@@ -1,16 +1,9 @@
 package dev.danvega.blog;
 
-import dev.danvega.blog.model.Author;
-import dev.danvega.blog.model.Comment;
-import dev.danvega.blog.model.Post;
-import dev.danvega.blog.repository.AuthorRepository;
-import dev.danvega.blog.repository.PostRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jdbc.core.mapping.AggregateReference;
+
 
 @SpringBootApplication
 public class Application {
