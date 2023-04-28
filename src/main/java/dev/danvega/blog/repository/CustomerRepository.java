0a85da6bd7f3f152dc280e2ca8d0b29c0b25f3f9
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-04-28 02:06:22 +0200

    test 03

--- src/main/java/dev/danvega/blog/repository/CustomerRepository.java
+++ src/main/java/dev/danvega/blog/repository/CustomerRepository.java
@@ -1,6 +1,6 @@
 package dev.danvega.blog.repository;
 
-/*
+
 import dev.danvega.blog.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     // additional methods can be defined here
-}*/
+}
