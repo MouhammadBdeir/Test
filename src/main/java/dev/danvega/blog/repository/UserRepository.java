45058284bdb4efd4200237cb2a0fda306f8349f3
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-06-05 16:15:01 +0200

    sicherer Zustand

--- src/main/java/dev/danvega/blog/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.danvega.blog.repository;
-
-import dev.danvega.blog.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
