6b19d9de2e3ee01f42e6d7d28ec8119ed62574ac
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-06-05 15:15:56 +0200

    add test with Admin site

---
+++ src/main/java/dev/danvega/blog/Config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package dev.danvega.blog.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin@example.com")
+                .password("{noop}admin123") // Passwort mit "{noop}" voranstellen, um NoOpPasswordEncoder zu verwenden (nicht für die Produktion empfohlen)
+                .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/admin")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
---
+++ src/main/java/dev/danvega/blog/controller/AdminController.java
@@ -0,0 +1,18 @@
+package dev.danvega.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+}
