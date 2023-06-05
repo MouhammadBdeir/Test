fc073a2c8432d306aaf7c69bd645a4296647040d
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-06-05 11:03:39 +0200

    remove jpa from poom 01

--- src/main/java/dev/danvega/blog/repository/PostRepository.java
+++ src/main/java/dev/danvega/blog/repository/PostRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface PostRepository extends CrudRepository<Post,Integer> {
-
     @Query("SELECT * FROM POST WHERE author = :id")
     List<Post> findByAuthor(Integer id);
-
 }
