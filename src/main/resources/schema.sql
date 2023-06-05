8f8875aa24754a6f364b8e1e5077f1246aeab7c8
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-06-05 13:24:40 +0200

    new test 04

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,16 @@
 
+-- Customer
+create table IF NOT EXISTS customer (
+    id serial primary key,
+    firstName varchar(100) not null,
+    lastName varchar(100) not null,
+    interest varchar(100) not null,
+    company varchar(100) not null,
+    anrede varchar(100) not null,
+    email varchar(255) not null,
+    phoneNumber varchar(20) not null,
+    message text not null
+    );
 CREATE TABLE IF NOT EXISTS feedback (
     id serial PRIMARY KEY ,
     message VARCHAR(1000),
