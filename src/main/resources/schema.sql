0320f619ffd486615bd24db76881c769b206fccd
ABOR3D1997 <omar.bd.1997@gmail.com>
2023-06-09 01:13:23 +0200

    add loginsystem 1.2.3

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,24 +1,23 @@
-
--- Customer
 CREATE TABLE IF NOT EXISTS customer (
-      id SERIAL PRIMARY KEY,
-      anrede VARCHAR(255),
-      first_name VARCHAR(255),
-      last_name VARCHAR(255),
-      email VARCHAR(255),
-      phone_number VARCHAR(255),
-      message TEXT,
-      interest VARCHAR(255),
-      company VARCHAR(255)
-);
+    id SERIAL PRIMARY KEY,
+    anrede VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    phone_number VARCHAR(255),
+    message TEXT,
+    interest VARCHAR(255),
+    company VARCHAR(255)
+    );
+
 CREATE TABLE IF NOT EXISTS feedback (
-    id serial PRIMARY KEY ,
+    id SERIAL PRIMARY KEY,
     message VARCHAR(1000),
     subject VARCHAR(255) NOT NULL DEFAULT ''
-);
+    );
 
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
+     id SERIAL PRIMARY KEY,
+     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(100) NOT NULL
- );
+    );
